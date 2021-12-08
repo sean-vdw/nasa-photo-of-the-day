278fe639e473f93e17412929e435240758c35c1a
sean-vdw <62733062+sean-vdw@users.noreply.github.com>
2021-12-08 13:31:03 -0800

    updated css of elements

--- src/Photo.js
+++ src/Photo.js
@@ -4,12 +4,30 @@ import axios from "axios";
 import "./App.css";
 
 
-
-
 function Photo() {
+    const [photo, setPhoto] = useState([]);
+
+    useEffect(() => {
+      const fetchPhoto = () => {
+        axios.get(`${BASE_URL}apod?api_key=${API_KEY}`)
+          .then(res => {
+            console.log(res.data);
+            setPhoto(res.data.url)
+          })
+          .catch(err => {
+            debugger
+          })
+      }
+      fetchPhoto()
+    }, [])
+
     return (
         <div className='photo-container'>
-            <img src=''></img>
+            {
+                <>
+                <img src={photo}></img>
+                </>
+            }
         </div>
     )
 }
